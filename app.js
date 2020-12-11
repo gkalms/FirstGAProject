fedd622b1478a726c94ae3b4b70ddadfef29d2a8
gkalms <georgina.kalms@gmail.com>
2020-12-11 20:18:45 +1100

    Changes to input click value

--- app.js
+++ app.js
@@ -2,4 +2,20 @@ console.log("hello");
 
 //Day of the week
 const d = new Date();
-$("h2").html("Today is the "+d.toLocaleDateString());  
+$("h2").html("Today is the " + d.toLocaleDateString());  
+
+//Get value from clicked button into text box
+$(".btn").on("click", (e) => {
+    console.log("button clicked")
+    let input = e.target.innerHTML;
+    console.log(input);
+    $("p").append(input);
+});
+
+// Can i use frestyle button to add happy picture to paragraph??
+
+//   Clear the text
+$("#clear").on("click", () => {
+    console.log("clear this field!");
+    $("p").html("");
+});
